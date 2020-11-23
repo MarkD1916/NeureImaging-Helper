b5e17ad8187963b29e27edd792a4a6488594f38f
MarkD <vmakd1916@gmail.com>
2020-11-23 13:02:04 +0300

    continue develope BD

--- Models/Model.py
+++ Models/Model.py
@@ -1,20 +1,39 @@
-class Session():
-    def __init__(self):
-        self.sessionID:int = 0
-        self.RatID: int = 0
-        self.drags: list[str] = [""]
-        self.coudBound: int = 0
-        self.medBoud: int = 0
-        self.latBound: int = 0
-        self.rostBound: int = 0
-        self.coords: list[[int,int]] = [[0,0]]
-        self.folderName: str = " "
-        self.date: str = " "
+class Experiments():
+    def __init__(self,ID,path, method):
+        self.ID: int = ID
+        self.path:str = path
+        self.method:str = method
 
 
 class Rat():
-    def __init__(self):
-        self.RatID:int = 0
-        self.mass: int = 0
-        self.name: str = " "
+    def __init__(self,ID,expID,name,mass):
+        self.ID:int = ID
+        self.mass: int = mass
+        self.name: str = name
+        self.expID = expID
+
+
+class Boundary():
+    def __init__(self,ID,rostral,caudal,medial,lateral,expID):
+        self.rostral:float = rostral
+        self.caudal:float = caudal
+        self.medial:float = medial
+        self.lateral:float = lateral
+        self.expID: int = expID
+        self.ID: int = ID
+
+class Drugs():
+    def __init__(self,expID,drugName,valve,ID):
+        self.expID:int = expID
+        self.drugName:str = drugName
+        self.valve:int = valve
+        self.ID: int = ID
+
+class Cords():
+    def __init__(self,expID,ID,xValue,yValue,region):
+        self.expID=expID
+        self.ID=ID
+        self.xValue=xValue
+        self.yValue=yValue
+        self.region=region
 
