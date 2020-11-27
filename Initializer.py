365469ff664641ee025c1e31b872601cafeeaea5
MarkD <vmakd1916@gmail.com>
2020-11-27 16:40:50 +0300

    Add some changes

--- Initializer.py
+++ Initializer.py
@@ -7,7 +7,7 @@ class InitializerDB():
     def __init__(self,mainDir):
         self.search = Searcher(mainDir=mainDir)
         self.parser = Parser()
-        self.experiments = {}
+        self.experiments = []
         self.drugs = []
         self.cords = {"ID":[],"expID":[],"xValue":[],"yValue":[],"region":[]}
         self.boundary = {"ID":[],"expID":[],"Rostral":[],"Caudal":[],"Medial":[],"Lateral":[]}
@@ -25,26 +25,19 @@ class InitializerDB():
 
     def setExperimentsDataInDB(self):
         pass
+
     def makeRowForTable(self,data, keys):
-        rows = []
-        id = 1
 
 
-        for expID in data.keys():
-            # TODO доделать
-            value = zip([data[expID][key] for key in keys])
-            print(value)
-            for drugs, valves in value: #data[expID]['Drug'],data[expID]['Valve']
-                row = [id, expID, drugs, valves]
-                rows.append(row)
-                id +=1
+
         return
+
     def getRoiLLMData(self):
         filesPath = self.search.searchRoiLLMFile()
         roiLLMData = self.parser.parseRoiLlmFile(filesPath)
         drugsTableData=self.makeRowForTable(roiLLMData,['Drug','Valve'])
-        print (drugsTableData)
         self.drugs = drugsTableData
-Init = InitializerDB("D:/N_img")
+
+Init = InitializerDB("/mnt/data/N_img")
 Init.getExperiments()
 Init.getRoiLLMData()
